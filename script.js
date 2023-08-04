dea1708cf937f0c911e939f6c265ccf96ac8d453
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-08-04 14:43:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-//your JS code here. If required.
+let p1 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("2");
+  }, 2000);
+});
+
+let p2 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("1");
+  }, 1000);
+});
+
+let p3 = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve("3");
+  }, 3000);
+});
+
+const tbody = document.getElementById("output");
+
+const startTime = performance.now();
+
+Promise.all([p1, p2, p3])
+  .then((value) => {
+    value.map((value, index) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>Promise ${index + 1}</td><td>${value}</td>`;
+      tbody.appendChild(row);
+    });
+  })
+  .then(() => {
+    const endTime = performance.now();
+
+    const row = document.createElement("tr");
+    row.innerHTML = `<td>Total Time</td><td>${
+      (endTime - startTime) / 1000
+    }</td>`;
+    tbody.appendChild(row);
+  });
